91bae6a2f74480fd510d61458b3fd2a3d941c328
Tony-Tan <363806348@qq.com>
2020-06-28 23:23:13 +0800

    dyna_q+ debug

--- TebularPlanningAndLearning/Dyna_Q_plus.py
+++ TebularPlanningAndLearning/Dyna_Q_plus.py
@@ -3,7 +3,6 @@ import numpy as np
 import collections
 import matplotlib.pyplot as plt
 import random
-import numpy as np
 
 
 def constant_factory(n):
@@ -22,31 +21,36 @@ class Agent:
         self.n = n
         self.total_step_num = 0
         self.kappa = kappa
-        self.initialize_q_and_model()
-
-    def initialize_q_and_model(self):
-        for state_iter in range(self.env.state_space.n):
-            if state_iter in self.env.termination:
-                continue
-            for action_iter in range(self.env.action_space.n):
-                new_state, reward, is_done, _ = self.env.step(action_iter, state=state_iter)
-                self.model[state_iter][action_iter] = [reward, new_state, 0]
-                self.model_state_action_list.append([state_iter, action_iter])
 
     def select_action(self, state):
         probability_distribution = self.policies[state]
         action = np.random.choice(self.env.action_space.n, 1, p=probability_distribution)
         return action[0]
 
+    def update_policy(self, state, epsilon):
+        # update policy
+        value_of_action_list = []
+        possible_action_num = self.env.action_space.n
+        for action_iter in range(possible_action_num):
+            value_of_action_list.append(self.value_state_action[(state, action_iter)])
+        value_of_action_list = np.array(value_of_action_list)
+        optimal_action = \
+            np.random.choice(np.flatnonzero(value_of_action_list == value_of_action_list.max()))
+        for action_iter in range(self.env.action_space.n):
+            if action_iter == optimal_action:
+                self.policies[state][action_iter] = 1 - epsilon + epsilon / possible_action_num
+            else:
+                self.policies[state][action_iter] = epsilon / possible_action_num
+
     def dyna_q_plus(self, number_of_episodes, alpha=0.1, gamma=1., epsilon=0.1):
+        steps_list = []
         for epi_iter in range(number_of_episodes):
             state = self.env.reset()
+            step_num = 0
             while True:
                 action = self.select_action(state)
-                new_state, reward, is_done, _ = self.env.step(action, state=state)
-                self.model[state][action] = [reward,new_state,self.total_step_num]
-                # next state-action return
-                # if not is_done:
+                new_state, reward, is_done, _ = self.env.step(action)
+                # q learning
                 q_state_next = []
                 for action_iter in range(self.env.action_space.n):
                     q_state_next.append(self.value_state_action[(new_state, action_iter)])
@@ -55,27 +59,16 @@ class Agent:
                 self.value_state_action[(state, action)] = \
                     q_state_current + alpha * (reward + gamma * q_state_next - q_state_current)
                 # update policy
-                value_of_action_list = []
-                possible_action_num = self.env.action_space.n
-                for action_iter in range(possible_action_num):
-                    value_of_action_list.append(self.value_state_action[(state, action_iter)])
-                value_of_action_list = np.array(value_of_action_list)
-                optimal_action = \
-                    np.random.choice(np.flatnonzero(value_of_action_list == value_of_action_list.max()))
-                for action_iter in range(self.env.action_space.n):
-                    if action_iter == optimal_action:
-                        self.policies[state][action_iter] = 1 - epsilon + epsilon / possible_action_num
-                    else:
-                        self.policies[state][action_iter] = epsilon / possible_action_num
-                # add into model
-                # self.model[state][action] = [reward, new_state]
-                # if [state, action] not in self.model_state_action_list:
-                #     self.model_state_action_list.append([state, action])
+                self.update_policy(state, epsilon)
+                # model learning
+                self.model[state][action] = [reward, new_state, self.total_step_num]
+                if [state, action] not in self.model_state_action_list:
+                    self.model_state_action_list.append([state, action])
                 # planning
                 if len(self.model_state_action_list) > 0:
                     for n_iter in range(self.n):
                         state_selected, action_selected = random.choice(self.model_state_action_list)
-                        reward_in_model, new_state_in_model,last_update_time = \
+                        reward_in_model, new_state_in_model, last_update_time = \
                             self.model[state_selected][action_selected]
                         reward_in_model = reward_in_model + self.kappa * np.sqrt(self.total_step_num - last_update_time)
                         q_state_next_in_model = []
@@ -86,23 +79,27 @@ class Agent:
                         self.value_state_action[(state_selected, action_selected)] = \
                             q_state_current_in_model + alpha * (reward_in_model + gamma * q_state_next_in_model -
                                                                 q_state_current_in_model)
-                        state = state_selected
+                        # update policy in planning
+                        self.update_policy(state_selected, epsilon)
                 if is_done:
                     break
                 self.total_step_num += 1
-        return
+                step_num += 1
+                state = new_state
+            steps_list.append(step_num)
+        return steps_list
 
 
 if __name__ == '__main__':
-    # env = GridWorld(6, [25,26,27,28,29], start_position=32, end_position_list=[5])
-    # steps_matrix = np.zeros((3,50))
-    # agent_n = [0,5,50]
-    # repeat_n_times = 50
+    # env = GridWorld(6, [25, 26, 27, 28, 29], start_position=31, end_position_list=[5])
+    # steps_matrix = np.zeros((3, 50))
+    # agent_n = [0, 5, 50]
+    # repeat_n_times = 10
     # for j in range(repeat_n_times):
     #     for i in range(3):
-    #         agent = Agent(env, n=agent_n[i])
-    #         steps = agent.dyna_q(50, alpha=0.1, gamma=0.95,epsilon=.3)
-    #         steps_matrix[i] += np.array(steps)
+    #         agent = Agent(env, n=agent_n[i], kappa=0)
+    #         steps = agent.dyna_q_plus(50, alpha=0.1, gamma=0.95,epsilon=.3)
+    #         steps_matrix[i] += steps
     # steps_matrix /= 50.
     # i = 0
     # for steps_array in steps_matrix:
@@ -110,12 +107,13 @@ if __name__ == '__main__':
     #     i += 1
     # plt.legend()
     # plt.show()
+
     env = GridWorld(6, [24, 25, 26, 27, 28], start_position=31, end_position_list=[5])
-    agent = Agent(env, n=10)
+    agent = Agent(env, n=10, kappa=0)
     dq_step_rewards_list = []
     steps = 0
     for i in range(100):
-        agent.dyna_q(1, alpha=0.1, gamma=0.95, epsilon=.3)
+        agent.dyna_q_plus(1, alpha=0.1, gamma=0.95, epsilon=.3)
         dq_step_rewards_list.append([agent.total_step_num, i])
         if i == 50:
             agent.env = GridWorld(6, [25, 26, 27, 28, 29], start_position=31, end_position_list=[5])
--- TebularPlanningAndLearning/model_changed.py
+++ TebularPlanningAndLearning/model_changed.py
@@ -5,12 +5,12 @@ import matplotlib.pyplot as plt
 
 if __name__ == '__main__':
     env = GridWorld(6, [24,25,26,27,28], start_position=31, end_position_list=[5])
-    episode_numbers = 200
+    episode_numbers = 400
     dqp_step_rewards_list = []
     dqp_steps = 0
     dq_step_rewards_list = []
     dq_steps = 0
-    agent_dqp = DQp.Agent(env, n=10)
+    agent_dqp = DQp.Agent(env, n=10, kappa=0.0001)
     agent_dq = DQp.Agent(env, n=10, kappa=0)
     dqp_reward = 0.0
     for i in range(episode_numbers):
@@ -18,12 +18,12 @@ if __name__ == '__main__':
         dqp_step_rewards_list.append([agent_dqp.total_step_num, i])
         agent_dq.dyna_q_plus(1, alpha=0.1, gamma=0.95, epsilon=.3)
         dq_step_rewards_list.append([agent_dq.total_step_num, i])
-        if i == episode_numbers/2:
+        if i == episode_numbers/4:
             agent_dqp.env = GridWorld(6, [25, 26, 27, 28], start_position=31, end_position_list=[5])
             agent_dq.env = GridWorld(6, [25, 26, 27, 28], start_position=31, end_position_list=[5])
     dqp_step_rewards_list = np.array(dqp_step_rewards_list)
-    plt.plot(dqp_step_rewards_list[:, 0], dqp_step_rewards_list[:, 1], label='Dyna_Q+')
+    plt.plot(dqp_step_rewards_list[:, 0] - dqp_step_rewards_list[0][0], dqp_step_rewards_list[:, 1], label='Dyna_Q+')
     dq_step_rewards_list = np.array(dq_step_rewards_list)
-    plt.plot(dq_step_rewards_list[:, 0], dq_step_rewards_list[:, 1], label='Dyna_Q')
+    plt.plot(dq_step_rewards_list[:, 0] - dq_step_rewards_list[0][0], dq_step_rewards_list[:, 1], label='Dyna_Q')
     plt.legend()
     plt.show()
