577792190f68ae42892839cf4e07470aa2938d1d
Tony <363806348@qq.com>
2020-07-15 20:06:21 +0800

    initialize on policy prediction with approximation

--- OnPolicyPrediction_Approximation/GradientMonteCarlo.py
+++ OnPolicyPrediction_Approximation/GradientMonteCarlo.py
@@ -11,14 +11,13 @@ def constant_factory(n):
 
 
 class LinearFunction:
-    def __init__(self,d):
+    def __init__(self, d):
         self.dim = d
-        self.weight = np.random.rand(self.dim + 1, 1)
+        self.weight = np.array([[0.],[0.]])
 
     def __call__(self, x):
-        x_ = np.ones([self.dim + 1, 1])
-        x_[1][0] = x
-        return np.dot(self.weight.transpose(), x_)[0][0]
+        x_ = np.array([[1],[x]])
+        return np.dot(self.weight.transpose(), x_)
 
 
 class Agent:
@@ -32,7 +31,7 @@ class Agent:
         action = np.random.choice(self.env.action_space.n, 1, p=probability_distribution)
         return action[0]
 
-    def MC_app(self, number_of_episodes, learning_rate, gamma=0.9):
+    def MC_app(self, number_of_episodes, learning_rate, gamma=1):
         for _ in range(number_of_episodes):
             episode = []
             state = self.env.reset()
@@ -49,21 +48,30 @@ class Agent:
             g = 0
             for i in range(len(episode)-1, -1, -1):
                 g = gamma*g + episode[i][0]
+                # g += episode[i][0]
+                episode[i][0] = g
+
+            for i in range(0, len(episode)):
                 s = episode[i][1]
                 if s is None:
                     continue
+                g = episode[i][0]
                 # s /= 1000.
-                delta_value = np.array([[1], [s]])
+                delta_value = np.array([[1.], [s]])
                 self.value_state.weight += learning_rate * (g - self.value_state(s)) * delta_value
 
 
 if __name__ == '__main__':
     env = RandomWalk1000()
     agent = Agent(env, 1)
-    agent.MC_app(1000, 2e-5)
-    x = np.arange(0, 1001, 1.)
-    y = np.arange(0, 1001, 1.)
+    agent.MC_app(1000000, 2e-6)
+    x = np.arange(1, 1001, 1.)
+    y = np.arange(1, 1001, 1.)
+    # for i in range(1, x.size, 2):
+    #     y[i-1] = agent.value_state(x[i-1] + 50)
+    #     y[i] = agent.value_state(x[i] - 50)
+    print(agent.value_state.weight)
     for i in range(x.size):
         y[i] = agent.value_state(x[i])
-    plt.plot(x,y)
+    plt.plot(x, y)
     plt.show()
