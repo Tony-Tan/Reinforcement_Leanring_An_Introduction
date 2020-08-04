3d7b3d4dd06b53b34e1635b980c3bf717e077c92
Tony <363806348@qq.com>
2020-08-04 20:34:03 +0800

    differential semi-gradient n-step Sarsa for estimating

--- OnPolicyControl_Approximation/Diff_Semi_gradientnStepSarsa.py
+++ OnPolicyControl_Approximation/Diff_Semi_gradientnStepSarsa.py
@@ -26,8 +26,9 @@ class Tabular:
 
 
 class Agent:
-    def __init__(self, environment):
+    def __init__(self, environment, n):
         self.env = environment
+        self.n = n
         self.value_of_state_action = Tabular([environment.customer_kind_number,
                                               environment.sever_num + 1,
                                               self.env.action_space.n])
@@ -42,39 +43,51 @@ class Agent:
     def running(self, max_step_num, alpha=0.01, beta=0.01, epsilon=0.1):
         state = self.env.reset()
         action = self.select_action(state)
-        # get reward R and next state S'
+        n_queue = collections.deque()
         for i in range(max_step_num):
             next_state, reward, is_done, _ = self.env.step(action)
-            next_action = self.select_action(next_state)
-            delta = reward - self.average_reward + self.value_of_state_action([next_state, next_action])\
-                - self.value_of_state_action([state, action])
-            self.average_reward += beta * delta
-            self.value_of_state_action.update_weight(delta, alpha, (state, action))
+            n_queue.append([state, action, reward, next_state])
+            if len(n_queue) < self.n + 1:
+                action = self.select_action(next_state)
+                state = next_state
+                continue
 
-            # update policy
-            value_of_action_list = []
-            for action_iter in range(self.env.action_space.n):
-                value_of_action_list.append(self.value_of_state_action((state, action_iter)))
-            value_of_action_list = np.array(value_of_action_list)
-            optimal_action = np.random.choice(
-                np.flatnonzero(value_of_action_list == value_of_action_list.max()))
-            for action_iter in range(self.env.action_space.n):
-                if action_iter == optimal_action:
-                    self.policies[state][
-                        action_iter] = 1 - epsilon + epsilon / self.env.action_space.n
-                else:
-                    self.policies[state][action_iter] = epsilon / self.env.action_space.n
-            # go to next step
-            action = self.select_action(next_state)
-            state = next_state
+            if len(n_queue) == self.n + 1:
+                state_tao_n = n_queue[-1][3]
+                action_tao_n = self.select_action(state_tao_n)
+                delta_r = 0
+                for rec_i in n_queue:
+                    delta_r += (rec_i[2] - self.average_reward)
+                state_2_update, action_2_update, reward, _ = n_queue.popleft()
+                delta = delta_r + self.value_of_state_action([state_tao_n, action_tao_n])\
+                    - self.value_of_state_action([state_2_update, action_2_update])
+                self.average_reward += beta * delta
+                self.value_of_state_action.update_weight(delta, alpha, (state_2_update, action_2_update))
+                # update policy
+                value_of_action_list = []
+                for action_iter in range(self.env.action_space.n):
+                    value_of_action_list.append(self.value_of_state_action((state_2_update, action_iter)))
+                value_of_action_list = np.array(value_of_action_list)
+                optimal_action = np.random.choice(
+                    np.flatnonzero(value_of_action_list == value_of_action_list.max()))
+                for action_iter in range(self.env.action_space.n):
+                    if action_iter == optimal_action:
+                        self.policies[state_2_update][
+                            action_iter] = 1 - epsilon + epsilon / self.env.action_space.n
+                    else:
+                        self.policies[state_2_update][action_iter] = epsilon / self.env.action_space.n
+                # go to next step
+                next_state = state_tao_n
+                action = self.select_action(next_state)
+                state = next_state
 
 
 if __name__ == '__main__':
     rewards_list = [1, 2, 4, 8]
     rewards_distribution = [0.25, 0.25, 0.25, 0.25]
     env = QueuingTask(rewards_list, rewards_distribution)
-    agent = Agent(env)
-    agent.running(100000)
+    agent = Agent(env, 0)
+    agent.running(1000000)
     for i in range(4):
         for j in range(1, 11):
             # if agent.policies[(i, j)][0] > agent.policies[(i, j)][1]:
