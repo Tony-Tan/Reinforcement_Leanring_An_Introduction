1f76a654b40482bdf1abee0615c27e607b48decc
Tony <363806348@qq.com>
2020-08-11 17:40:17 +0800

    draw td(lambda) algorithm figure

--- EliqibilityTraces/td_lambda.py
+++ EliqibilityTraces/td_lambda.py
@@ -47,10 +47,43 @@ class Agent:
 
 if __name__ == '__main__':
     env = RandomWalk(19)
-    agent = Agent(env)
-    agent.estimating(10, 0.8, 0.2, 0.9)
-    value_of_state = []
-    for i_state in range(1, env.state_space.n - 1):
-        value_of_state.append(agent.value_of_state[i_state])
-    plt.plot(value_of_state)
+    alpha_list = [i / 20. for i in range(1, 21)]
+    lambda_list = [1., 0.99, 0.975, 0.95, 0.9, 0.8, 0.4, 0]
+    ground_truth = [(-1. + i / 9.) for i in range(0, 19)]
+    ground_truth = np.array(ground_truth)
+    average_times = 100
+    for j in range(len(lambda_list)):
+        mse = []
+        for alpha_i in range(0, len(alpha_list)):
+            print(alpha_i, alpha_list[alpha_i])
+            mse_current = 0
+            for aver in range(average_times):
+                agent = Agent(env)
+                agent.estimating(10, lambda_list[j], alpha_list[alpha_i], 0.9)
+                value_of_state = []
+                for i_state in range(1, env.state_space.n - 1):
+                    value_of_state.append(agent.value_of_state[i_state])
+                value_of_state = np.array(value_of_state)
+                mse_current += np.sum((value_of_state - ground_truth[1:env.state_space.n - 1]) *
+                                      (value_of_state - ground_truth[1:env.state_space.n - 1])) / 17.
+
+            mse.append(np.sqrt(mse_current / float(average_times)))
+        max_i = -1
+        for mse_i in mse:
+            if mse_i < 1.0:
+                max_i += 1
+            else:
+                break
+
+        plt.plot(alpha_list[0:max_i], mse[0:max_i], label='$\\lambda = $' + str(lambda_list[j]))
+    plt.legend()
     plt.show()
+
+    # env = RandomWalk(19)
+    # agent = Agent(env)
+    # agent.estimating(10, 1, 1, 0.9)
+    # value_of_state = []
+    # for i_state in range(1, env.state_space.n - 1):
+    #     value_of_state.append(agent.value_of_state[i_state])
+    # plt.plot(value_of_state)
+    # plt.show()
