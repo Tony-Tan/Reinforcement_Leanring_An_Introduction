fd6d60baee8f624e304b71329b5e1f45810a963c
Tony <363806348@qq.com>
2020-08-19 15:42:13 +0800

    REINFORCE with baseline

--- PolicyApproximation/REINFORCE_with_baseline.py
+++ PolicyApproximation/REINFORCE_with_baseline.py
@@ -19,10 +19,22 @@ class Linear_policy:
         return np.array([0, y]) / self.__call__(x, y)
 
 
+class Linear_State_Value:
+    def __init__(self):
+        self.weight = np.zeros(2)
+
+    def __call__(self, x):
+        return x * self.weight[0] + self.weight[1]
+
+    def derivative(self,x):
+        return np.array([x,1.])
+
+
 class Agent:
     def __init__(self, env):
         self.env = env
         self.policy = Linear_policy()
+        self.state_value = Linear_State_Value()
 
     def select_action(self, state):
         probability_distribution = []
@@ -35,7 +47,7 @@ class Agent:
         action = np.random.choice(env.action_space.n, 1, p=probability_distribution)
         return action[0]
 
-    def play(self, number_of_episodes, alpha, gamma):
+    def play(self, number_of_episodes, alpha_theta, alpha_w, gamma):
         reward_per_episode = []
         for eps_iter in range(number_of_episodes):
             reward_sum = 0
@@ -57,9 +69,11 @@ class Agent:
             gamma_t = 1.
             for epd_i in range(len(episode) - 1):
                 state, action, r = episode[epd_i]
-                g = total_g
-                theta = self.policy.derivative_ln(state, action)
-                self.policy.weight += alpha * gamma_t * g * theta
+                delta = total_g - self.state_value(state)
+                delta_ln_theta = self.policy.derivative_ln(state, action)
+                delta_state_value = self.state_value.derivative(state)
+                self.state_value.weight += alpha_w * delta * delta_state_value
+                self.policy.weight += alpha_theta * gamma_t * delta * delta_ln_theta
                 gamma_t *= gamma
                 total_g -= r
                 total_g /= gamma
@@ -73,29 +87,21 @@ if __name__ == '__main__':
     # for i in range(0, 1):
 
     episode_len = 1000
-    repeat_time = 10
+    repeat_time = 30
     steps = np.zeros(episode_len)
     for i in range(repeat_time):
         print('repeat time ' + str(i))
         env = ShortCorridor()
         agent = Agent(env)
-        steps += agent.play(episode_len, 2e-7, 1)
-    plt.plot(steps / repeat_time, alpha=0.7, label='$\\alpha=2e-7$')
+        steps += agent.play(episode_len, 2e-8, 0, 1)
+    plt.plot(steps / repeat_time, alpha=0.7, label='$\\alpha_w=0$')
 
-    # steps = np.zeros(episode_len)
-    # for i in range(repeat_time):
-    #     print('repeat time ' + str(i))
-    #     env = ShortCorridor()
-    #     agent = Agent(env)
-    #     steps += agent.play(episode_len, 2e-8, 1)
-    # plt.plot(steps / repeat_time, alpha=0.7, label='$\\alpha=2e-8$')
-    #
-    # steps = np.zeros(episode_len)
-    # for i in range(repeat_time):
-    #     print('repeat time ' + str(i))
-    #     env = ShortCorridor()
-    #     agent = Agent(env)
-    #     steps += agent.play(episode_len, 2e-9, 1)
-    # plt.plot(steps / repeat_time, alpha=0.7, label='$\\alpha=2e-9$')
+    steps = np.zeros(episode_len)
+    for i in range(repeat_time):
+        print('repeat time ' + str(i))
+        env = ShortCorridor()
+        agent = Agent(env)
+        steps += agent.play(episode_len, 2e-7, 2e-7, 1)
+    plt.plot(steps / repeat_time, alpha=0.7, label='$\\alpha_w=2^{-5}$')
     plt.legend()
     plt.show()
